b0deb9c8c02ed6fe62be02600e2297da694e0226
vasusadariya <sadariyavasu5@gmail.com>
2025-01-27 22:04:36 +0530

    initial commit-project setup

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return  <div className="min-h-screen bg-[#FFFFF4]">
+    <nav className="mx-auto border-b-2 border-[#151616] py-8 px-8 bg-[#FFFFFFE6] backdrop-blur-md">
+      <div className=""> 
+      </div>
+    </nav>
+  </div>
+}
